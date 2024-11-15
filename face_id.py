7aa7623e0a6c85d0eef4cd2b9757f695fd5eeb3e
jongseo05 <jourxy05@naver.com>
2024-11-15 16:53:39 +0900

    implemented verify_face

--- face_id.py
+++ face_id.py
@@ -1,11 +1,40 @@
 from deepface import DeepFace
+from scipy.spatial.distance import cosine
 
 user_database = {}
 
 def register_face(user_id, img_path):
     embedding_obj = DeepFace.represent(img_path, model_name="Facenet")
     embedding = embedding_obj[0]['embedding']
+
     user_database[user_id] = embedding
     print(f"User {user_id} has been registered.")
 
-register_face("user1"  ,"user1.jpg")
+def verify_face(user_id, img_path):
+    registered_embedding = user_database[user_id]
+
+    embedding_obj = DeepFace.represent(img_path, model_name="Facenet")
+    embedding = embedding_obj[0]['embedding']
+
+    similarity = 1 - cosine(registered_embedding, embedding)
+    threshold = 0.8
+
+    if similarity > threshold:
+        print("Verification successful!")
+        return True
+    else:
+        print("Verification failed.")
+        return False
+
+register_face("user1", "user1.jpg")
+
+is_verified = verify_face("user1", "attempt1.jpg")
+print("Face verified:", is_verified)
+print()
+
+is_verified = verify_face("user1", "attempt2.jpg")
+print("Face verified:", is_verified)
+print()
+
+is_verified = verify_face("user1", "attempt3.jpg")
+print("Face verified:", is_verified)
